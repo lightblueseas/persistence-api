022ae4be2fad8c75d25cb0fb6b36c660da0927ac
astrapi69 <asterios.raptis@gmx.net>
2015-11-13 18:31:16 +0100

    null check

--- business-api/src/main/java/de/alpharogroup/db/entitymapper/AbstractEntityBOMapper.java
+++ business-api/src/main/java/de/alpharogroup/db/entitymapper/AbstractEntityBOMapper.java
@@ -13,81 +13,82 @@ import de.alpharogroup.lang.TypeArgumentsUtils;
 import lombok.Getter;
 
 /**
- * The abstract class {@link AbstractEntityBOMapper} provides an base implementation for mapping entities to business objects and back.
+ * The abstract class {@link AbstractEntityBOMapper} provides an base
+ * implementation for mapping entities to business objects and back.
  *
- * @param <E> the element type
- * @param <BO> the generic type
+ * @param <E>
+ *            the element type
+ * @param <BO>
+ *            the generic type
  */
 public abstract class AbstractEntityBOMapper<E extends BaseEntity<?>, BO extends BusinessObject<?>>
-	implements
-		EntityBOMapper<E, BO>
-{
+		implements EntityBOMapper<E, BO> {
 
 	/**
 	 * The mapper instance.
 	 */
-	
+
 	/**
 	 * Gets the mapper.
 	 *
 	 * @return the mapper
 	 */
-	@Getter 
+	@Getter
 	private final Mapper mapper;
 
 	/** The entity class. */
 	@SuppressWarnings("unchecked")
-	
+
 	/**
 	 * Gets the entity class.
 	 *
 	 * @return the entity class
 	 */
 	@Getter
-	private final Class<E> entityClass = (Class<E>)TypeArgumentsUtils.getTypeArgument(
-		AbstractEntityBOMapper.class, getClass(), 0);
+	private final Class<E> entityClass = (Class<E>) TypeArgumentsUtils.getTypeArgument(AbstractEntityBOMapper.class,
+			getClass(), 0);
 
 	/** The business object class. */
 	@SuppressWarnings("unchecked")
-	
+
 	/**
 	 * Gets the business object class.
 	 *
 	 * @return the business object class
 	 */
 	@Getter
-	private final Class<BO> businessObjectClass = (Class<BO>)TypeArgumentsUtils.getTypeArgument(
-		AbstractEntityBOMapper.class, getClass(), 1);
-	
+	private final Class<BO> businessObjectClass = (Class<BO>) TypeArgumentsUtils
+			.getTypeArgument(AbstractEntityBOMapper.class, getClass(), 1);
+
 	/**
 	 * Instantiates a new {@link AbstractEntityBOMapper}.
 	 */
-	public AbstractEntityBOMapper()
-	{
+	public AbstractEntityBOMapper() {
 		this(Collections.<String> emptyList());
 	}
-	
+
 	/**
 	 * Instantiates a new {@link AbstractEntityBOMapper}.
 	 *
-	 * @param mappingFiles the mapping files
+	 * @param mappingFiles
+	 *            the mapping files
 	 */
-	public AbstractEntityBOMapper(final List<String> mappingFiles)
-	{
+	public AbstractEntityBOMapper(final List<String> mappingFiles) {
 		mapper = newMapper(mappingFiles);
 	}
 
 	/**
-	 * Factory method for creating the new {@link Mapper} for the mapping process with the given mapping files list. This method is invoked
-	 * in the constructor and can be overridden so users can provide their
-	 * own mapping process. 
+	 * Factory method for creating the new {@link Mapper} for the mapping
+	 * process with the given mapping files list. This method is invoked in the
+	 * constructor and can be overridden so users can provide their own mapping
+	 * process.
 	 * 
-	 * @param mappingFiles the mapping files
+	 * @param mappingFiles
+	 *            the mapping files
 	 *
 	 * @return the new {@link Mapper} for the mapping process.
 	 */
-	public Mapper newMapper(final List<String> mappingFiles)
-	{
+	public Mapper newMapper(final List<String> mappingFiles) {
 		return new DozerBeanMapper(mappingFiles);
 	}
 
@@ -95,23 +96,23 @@ public abstract class AbstractEntityBOMapper<E extends BaseEntity<?>, BO extends
 	 * {@inheritDoc}
 	 */
 	@Override
-	public BO toBusinessObject(final E entity)
-	{
-		return this.mapper.map(entity, getBusinessObjectClass());
+	public BO toBusinessObject(final E entity) {
+		if (entity != null) {
+			return this.mapper.map(entity, getBusinessObjectClass());
+		}
+		return null;
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public List<BO> toBusinessObjects(final List<E> entities)
-	{
+	public List<BO> toBusinessObjects(final List<E> entities) {
 		final List<BO> businessObjects = new ArrayList<>();
-		if((entities != null) && !entities.isEmpty()) {
-			for (final E entity : entities)
-			{
+		if ((entities != null) && !entities.isEmpty()) {
+			for (final E entity : entities) {
 				businessObjects.add(toBusinessObject(entity));
-			}			
+			}
 		}
 		return businessObjects;
 	}
@@ -120,12 +121,10 @@ public abstract class AbstractEntityBOMapper<E extends BaseEntity<?>, BO extends
 	 * {@inheritDoc}
 	 */
 	@Override
-	public List<E> toEntities(final List<BO> businessObjects)
-	{
+	public List<E> toEntities(final List<BO> businessObjects) {
 		final List<E> entities = new ArrayList<>();
-		if((businessObjects != null) && !businessObjects.isEmpty()) {
-			for (final BO businessObject : businessObjects)
-			{
+		if ((businessObjects != null) && !businessObjects.isEmpty()) {
+			for (final BO businessObject : businessObjects) {
 				entities.add(toEntity(businessObject));
 			}
 		}
@@ -136,9 +135,11 @@ public abstract class AbstractEntityBOMapper<E extends BaseEntity<?>, BO extends
 	 * {@inheritDoc}
 	 */
 	@Override
-	public E toEntity(final BO businessObject)
-	{
-		return this.mapper.map(businessObject, getEntityClass());
+	public E toEntity(final BO businessObject) {
+		if(businessObject != null) {
+			return this.mapper.map(businessObject, getEntityClass());
+		}
+		return null;
 	}
 
 }
