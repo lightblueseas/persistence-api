d3ad4a1bd5ecb4ee480a91292ea13ca058a99551
astrapi69 <asterios.raptis@gmx.net>
2016-03-10 12:09:18 +0100

    created new attribute converter for convert a LocalDateTime to Date

---
+++ data-api/src/main/java/de/alpharogroup/db/converter/LocalDateTimeConverter.java
@@ -0,0 +1,36 @@
+package de.alpharogroup.db.converter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * The class {@link LocalDateTimeConverter} is an attribute converter for {@link LocalDateTime} to {@link Date}.
+ *
+ * @param LocalDateTime the type of the entity attribute
+ * @param Date the type of the database column
+ */
+@Converter(autoApply = true)
+public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Date> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Date convertToDatabaseColumn(final LocalDateTime date) {
+        final Instant instant = Instant.from(date);
+        return Date.from(instant);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public LocalDateTime convertToEntityAttribute(final Date value) {
+        final Instant instant = value.toInstant();
+        return LocalDateTime.from(instant);
+    }
+}
