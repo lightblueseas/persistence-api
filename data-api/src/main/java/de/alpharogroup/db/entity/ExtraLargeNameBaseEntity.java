1bd1c90cf3a0e2d0cdb184ef92fda735bc57e85e
Asterios Raptis <asterios.raptis@gmx.net>
2016-06-04 11:53:09 +0200

    New version of persistence-api

---
+++ data-api/src/main/java/de/alpharogroup/db/entity/ExtraLargeNameBaseEntity.java
@@ -0,0 +1,36 @@
+package de.alpharogroup.db.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * The class {@link NameBaseEntity} is a base entity for a table with a single value.
+ *
+ * @param <T> the generic type of the id
+ */
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Access(AccessType.FIELD)
+@Getter
+@Setter
+@NoArgsConstructor
+public abstract class ExtraLargeNameBaseEntity<T extends Serializable> extends VersionableBaseEntity<T>
+{
+
+	/**  The serial Version UID. */
+	private static final long serialVersionUID = 1L;
+
+	/** The name. */
+	@Column( name = "name", length = 1024  )
+	private String name;
+}
