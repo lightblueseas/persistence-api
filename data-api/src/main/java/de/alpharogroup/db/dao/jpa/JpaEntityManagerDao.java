5263406040bea9ba3f2cd07ea301bf7852eeacda
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2017-09-07 18:42:18 +0200

    Update JpaEntityManagerDao.java

--- data-api/src/main/java/de/alpharogroup/db/dao/jpa/JpaEntityManagerDao.java
+++ data-api/src/main/java/de/alpharogroup/db/dao/jpa/JpaEntityManagerDao.java
@@ -22,6 +22,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
