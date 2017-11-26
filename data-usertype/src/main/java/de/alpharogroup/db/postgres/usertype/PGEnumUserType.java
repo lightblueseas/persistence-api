c77f6037d1f13802704ab3cdc352460aa32e88c1
Asterios Raptis <asterios.raptis@gmx.net>
2017-11-26 13:48:27 +0100

    added hbm 5 version to javadoc

--- data-usertype/src/main/java/de/alpharogroup/db/postgres/usertype/PGEnumUserType.java
+++ data-usertype/src/main/java/de/alpharogroup/db/postgres/usertype/PGEnumUserType.java
@@ -35,7 +35,7 @@ import lombok.extern.slf4j.Slf4j;
  * The class {@link PGEnumUserType} maps string to enum and back. Can be used only with Postgres
  * database and hibernate.
  *
- * Note: Only use with Postgres and hibernate!!!
+ * Note: Only use with Postgres and hibernate 5!!!
  *
  * @author Asterios Raptis
  */
