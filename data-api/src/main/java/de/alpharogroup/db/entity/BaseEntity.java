f10565c2347d4385727e88070b600f4994f896c9
James Phelps <tyler.durden69@gmx.de>
2018-01-12 15:26:02 +0100

    Update BaseEntity.java

--- data-api/src/main/java/de/alpharogroup/db/entity/BaseEntity.java
+++ data-api/src/main/java/de/alpharogroup/db/entity/BaseEntity.java
@@ -70,12 +70,12 @@ public abstract class BaseEntity<PK extends Serializable> implements Serializabl
 	@Override
 	public boolean equals(Object obj)
 	{
-		if (this == obj)
-			return true;
 		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
+		if (this == obj)
+			return true;
 		@SuppressWarnings("rawtypes")
 		final BaseEntity other = (BaseEntity)obj;
 		if (id == null)
