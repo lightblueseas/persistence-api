f5a17cb76f289666f18b1ed0a29439ae2dc6d434
Asterios Raptis <asterios.raptis@gmx.net>
2018-03-03 16:12:16 +0100

    created new versionable validation entity

--- base-entities/src/main/java/de/alpharogroup/db/entity/validation/ValidatableEntity.java
+++ base-entities/src/main/java/de/alpharogroup/db/entity/validation/ValidatableEntity.java
@@ -18,33 +18,33 @@ package de.alpharogroup.db.entity.validation;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
-import de.alpharogroup.db.entity.version.VersionableBaseEntity;
+import de.alpharogroup.db.entity.BaseEntity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
- * The class {@link ValidatableBaseEntity} is a base entity and has a validFrom property and a
- * validTill property for restricting an entity in a range in which it is valid.
+ * The class {@link ValidatableEntity} is a base entity and has a validFrom property and a
+ * validTill property for restricting an entity in a time range in which it is valid.
  *
  * @param <T>
  *            the generic type of the id
  */
-@MappedSuperclass
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-@Access(AccessType.FIELD)
+@Entity
+@Table(name = "validation")
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class ValidatableBaseEntity<T extends Serializable> extends VersionableBaseEntity<T>
+@Builder(toBuilder = true)
+public class ValidatableEntity<T extends Serializable> extends BaseEntity<T>
 {
 
 	/** The serial Version UID. */
